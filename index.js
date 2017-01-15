b76b730f849b44b9a5e867edea36635d4291626a
Nicholas Kostelnik <nkostelnik@gmail.com>
2017-01-16 07:54:18 +0800

    use docker compose

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 var mailin = require('mailin');
   
-mailin.start({ 
-  port: process.env.NODE_ENV === 'production' ? 25 : 2225, 
-  webhook: process.env.WEB_HOOK
-});
+mailin.start({ port: process.env.PORT, webhook: process.env.WEB_HOOK });
