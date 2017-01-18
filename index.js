96eaa86490a9a5fa403715ac7afaad98a163f5b1
Nicholas Kostelnik <nkostelnik@gmail.com>
2017-01-18 09:59:29 +0800

    a way to test

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-var mailin = require('mailin');
-  
-mailin.start({ port: process.env.PORT, webhook: process.env.WEB_HOOK });
+require('mailin').start({ port: process.env.PORT, webhook: process.env.WEB_HOOK });
