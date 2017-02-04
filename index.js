e6631a7763ac8a0c76cbe5af05e77ea79b0e8680
Nicholas Kostelnik <nkostelnik@gmail.com>
2017-02-04 14:51:33 +0800

    raven

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-require('mailin').start({ port: process.env.PORT, webhook: process.env.WEB_HOOK });
+const Raven = require('raven')
+
+Raven.config(process.env.RAVEN_DSN).install()
+
+require('mailin')
+ .start({ port: process.env.PORT, webhook: process.env.WEB_HOOK })
+ .on('error', (err) => Raven.captureException(err))
